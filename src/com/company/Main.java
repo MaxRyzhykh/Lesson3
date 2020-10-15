7cbb3e1aa2275d4353aa24259ff301c31962c079
MaxRyzhykh <maxim.ryzhykh@gmail.com>
2020-10-15 22:22:11 +0300

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,8 +12,37 @@ public class Main {
             System.out.println("Лето");
         } else if (8 < month && month < 12) {
             System.out.println("Осень");
+        } else System.out.println("Некорректное число месяца");
+
+        switch (month) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Некорректное число месяца");
+                break;
         }
 
-
+        int array[] = {0,1,2,3,4,5,6,7,8,9};
+        for (int i = (array.length)-1; i > -1; i--) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
